f3d8c34803e87302e40f0fc6af19e7cb5e1d1e35
aoi530 <minto5.1999@gmail.com>
2024-11-12 12:40:12 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ $(function () {
             let scroll = $(window).scrollTop();
             let target = $(this).offset().top;
             let windowHeight = $(window).height();
-            if (scroll > target - windowHeight + 50) {
+            if (scroll > target - windowHeight + 200) {
                 $(this).css("opacity", "1");
                 $(this).css("transform", "translateY(0)");
             }
